8a2b84284d1527c6565c27a11b41f10b271d6121
HazmatDrone <drone@hazmatdr.one>
2021-11-08 15:49:37 +1100

    WIP

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # tmux-pacman-notifier
 Pacman update notifier for TMUX.
+## Usage
+Add `#{pacman_updates}` to your `status-left` or `status-right`, e.g:
+```bash
+set -g status-right '#{pacman_updates}
+```
+
+## Customisation
+You can set a custom format for your notifier by setting `pacman-update-format`.
+You can get the number of updates with `#{pacman}`, `#{aur}`, and `#{combined}`.
+You can use colours with `#[fg=colourX,bg=colourX]`.
+By default this value is `#[fg=colour3,bg=colour0]C#[fg=color1] #{combined} `.
+E.g:
+```bash
+set -g @pacman-update-format '#[fg=colour15,bg=colour0] There are #[fg=colour3]#{combined}#[fg=colour15] updates available'
+```
+
+You can set when the notifier should show by setting `pacman-update-show-on`.
+Valid options are `any`, `pacman`, `aur`, and `always`. E.g:
+```bash
+set -g @pacman-update-show-on 'always'
+
+```
+
+## Installation with TPM
+1. Add the following line to `~/.tmux.conf`:
+```bash
+set -g @plugin 'HazmatDrone/tmux-pacman-notifier'
+```
+2. Refresh your plugins (default `prefix + I`)
+
+## License
+[MIT](/aaronpowell/tmux-weather/blob/master/LICENSE)
--- getupdates.sh
+++ getupdates.sh
@@ -5,14 +5,39 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$CURRENT_DIR/helpers.sh"
 
 print_updates() {
-	local format=$(get_tmux_option @pacman-update-format "")
-	local prefix=$(get_tmux_option @pacman-update-prefix " ")
-	local suffix=$(get_tmux_option @pacman-update-suffix " ")
-	local updates="$(checkupdates | wc -l)"
-	printf "$format/$prefix/$updates/$suffix"
+	local format=$(get_tmux_option @pacman-update-format "#[fg=colour3,bg=colour0]C#[fg=color1] #{combined} ")
+	local showon=$(get_tmux_option @pacman-update-show-on "any")
 
-	#if [[ updates > 0 ]]; then
-	#fi
+	local pac_sub="\#{pacman}"
+	local aur_sub="\#{aur}"
+	local combined_sub="\#{combined}"
+
+	nb_pac=$(checkupdates | wc -l)
+	if [ -x /usr/bin/yay ]; then
+		nb_aur=$(yay -Qua | wc -l)
+	elif [ -x /usr/bin/trizen ]; then
+		nb_aur=$(trizen -Qua | wc -l)
+	elif [ -x /usr/bin/pacaur ]; then
+		nb_aur=$(pacaur -Qua | awk '$2 == "aur" {print $3 $4 $5 $6}' | wc -l)
+	elif [ -x /usr/bin/yaourt ]; then
+		nb_aur=$(yaourt -Qua | grep "^aur/" | wc -l)
+	else
+		nb_aur=0
+	fi
+
+	nb_combined=$(($nb_pac + $nb_aur))
+	if [[ \
+	($nb_combined -gt 0 && $showon=="any") \
+	|| ($nb_pac -gt 0 && $showon=="pacman") \
+	|| ($nb_aur -gt 0 && $showon=="aur") \
+	|| $showon=="always" \
+	]]; then
+		output="${format/$pac_sub/$nb_pac}"
+		output="${output/$aur_sub/$nb_aur}"
+		printf "${output/$combined_sub/$nb_combined}"
+	else
+		printf ""
+	fi
 }
 
 main() {
